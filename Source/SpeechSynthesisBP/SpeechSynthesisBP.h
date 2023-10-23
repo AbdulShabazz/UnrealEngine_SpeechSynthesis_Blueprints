7070d5ff7d981ef90d12c1e9b57addad73bd32a4
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-10-22 20:49:02 -0400

    Update SpeechSynthesisBP.h

--- Source/SpeechSynthesisBP/SpeechSynthesisBP.h
+++ Source/SpeechSynthesisBP/SpeechSynthesisBP.h
@@ -4,43 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Kismet/BlueprintFunctionLibrary.h"
 
-//#include "SpeechSynthesisBP.generated.h"
-
-UCLASS(ClassGroup=(Custom), meta = (BlueprintSpawnableComponent))
-class SPEECHSYNTHESISBP_API USpeechSynthesisComponent : public UActorComponent
+class USpeechSynthesisComponent : public UActorComponent
 {
-	//GENERATED_BODY()
 
 public:
-
-	/** Returns a string cintaining the node's title */
-
-	UFUNCTION(BlueprintCallable, Category = "SpeechSynthesis")
-
-	static FString GetNodeTitle()
-	{
-		return FString("Speech Synthesis Blueprint");
-	}
-
-	/** Returns a string containing the node's tooltip */
-
-	UFUNCTION(BlueprintCallable, Category = "SpeechSynthesis")
-
-	static FString GetNodeTooltip()
-	{
-		return FString("Speech Synthesis Blueprint");
-	}
-
-	/** Execute the node */
-
-	UFUNCTION(BlueprintCallable, Category = "SpeechSynthesis")
-
-	static void ExecuteNode()
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Speech Synthesis Blueprint"));
-	}
 
 };
 
