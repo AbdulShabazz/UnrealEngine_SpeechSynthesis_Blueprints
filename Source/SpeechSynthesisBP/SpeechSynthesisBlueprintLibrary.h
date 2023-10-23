44faea5e206db4b58f95b7754863a13841dfd099
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-10-22 21:09:34 -0400

    Update SpeechSynthesisBlueprintLibrary.h

--- Source/SpeechSynthesisBP/SpeechSynthesisBlueprintLibrary.h
+++ Source/SpeechSynthesisBP/SpeechSynthesisBlueprintLibrary.h
@@ -6,6 +6,38 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "SpeechSynthesisBlueprintLibrary.generated.h"
 
+class FVoice
+{
+
+public:
+
+	FVoice()
+	{
+
+	};
+
+	~FVoice()
+	{
+
+	};
+};
+
+class IPASpeechSynthesis
+{
+
+public:
+
+	IPASpeechSynthesis()
+	{
+
+	};
+
+	~IPASpeechSynthesis()
+	{
+
+	};
+};
+
 /**
  * 
  */
@@ -18,51 +50,58 @@ public:
 
 	/** Activates speech synthesis channel phoneme functionality 'h' */
 	UFUNCTION(BlueprintCallable, Category = TextToSpeech, meta = ( DisplayName = "IPASpeechSynthesis_h" ))
-	void IPASpeechSynthesis_h(UPARAM(DisplayName = "Channel Id") FName InChannelId)
+	IPASpeechSynthesis IPASpeechSynthesis_h(UPARAM(DisplayName = "IPASpeechSynthesis") IPASpeechSynthesis InIPASpeechSynthesisObj, 
+		UPARAM(DisplayName = "Voice") FVoice InVoice)
 	{
-
+		return InIPASpeechSynthesisObj;
 	};
 
 	/** Activates speech synthesis channel phoneme functionality 'e' */
 	UFUNCTION(BlueprintCallable, Category = TextToSpeech, meta = ( DisplayName = "IPASpeechSynthesis_e" ))
-	void IPASpeechSynthesis_e(UPARAM(DisplayName = "Channel Id") FName InChannelId)
+	IPASpeechSynthesis IPASpeechSynthesis_e(UPARAM(DisplayName = "IPASpeechSynthesis") IPASpeechSynthesis InIPASpeechSynthesisObj, 
+		UPARAM(DisplayName = "Voice") FVoice InVoice)
 	{
-
+		return InIPASpeechSynthesisObj;
 	};
 
 	/** Activates speech synthesis channel phoneme functionality 'l' */
 	UFUNCTION(BlueprintCallable, Category = TextToSpeech, meta = ( DisplayName = "IPASpeechSynthesis_l" ))
-	void IPASpeechSynthesis_l(UPARAM(DisplayName = "Channel Id") FName InChannelId)
+	IPASpeechSynthesis IPASpeechSynthesis_l(UPARAM(DisplayName = "IPASpeechSynthesis") IPASpeechSynthesis InIPASpeechSynthesisObj, 
+		UPARAM(DisplayName = "Voice") FVoice InVoice)
 	{
-
+		return InIPASpeechSynthesisObj;
 	};
 
 	/** Activates speech synthesis channel phoneme functionality 'o' */
 	UFUNCTION(BlueprintCallable, Category = TextToSpeech, meta = ( DisplayName = "IPASpeechSynthesis_o" ))
-	void IPASpeechSynthesis_o(UPARAM(DisplayName = "Channel Id") FName InChannelId)
+	IPASpeechSynthesis IPASpeechSynthesis_o(UPARAM(DisplayName = "IPASpeechSynthesis") IPASpeechSynthesis InIPASpeechSynthesisObj, 
+		UPARAM(DisplayName = "Voice") FVoice InVoice)
 	{
-
+		return InIPASpeechSynthesisObj;
 	};
 
 	/** Activates speech synthesis channel phoneme functionality 'w' */
 	UFUNCTION(BlueprintCallable, Category = TextToSpeech, meta = ( DisplayName = "IPASpeechSynthesis_w" ))
-	void IPASpeechSynthesis_w(UPARAM(DisplayName = "Channel Id") FName InChannelId)
+	IPASpeechSynthesis IPASpeechSynthesis_w(UPARAM(DisplayName = "IPASpeechSynthesis") IPASpeechSynthesis InIPASpeechSynthesisObj, 
+		UPARAM(DisplayName = "Voice") FVoice InVoice)
 	{
-
+		return InIPASpeechSynthesisObj;
 	};
 
 	/** Activates speech synthesis channel phoneme functionality 'r' */
 	UFUNCTION(BlueprintCallable, Category = TextToSpeech, meta = ( DisplayName = "IPASpeechSynthesis_r" ))
-	void IPASpeechSynthesis_r(UPARAM(DisplayName = "Channel Id") FName InChannelId)
+	IPASpeechSynthesis IPASpeechSynthesis_r(UPARAM(DisplayName = "IPASpeechSynthesis") IPASpeechSynthesis InIPASpeechSynthesisObj, 
+		UPARAM(DisplayName = "Voice") FVoice InVoice)
 	{
-
+		return InIPASpeechSynthesisObj;
 	};
 
 	/** Activates speech synthesis channel phoneme functionality 'd' */
 	UFUNCTION(BlueprintCallable, Category = TextToSpeech, meta = ( DisplayName = "IPASpeechSynthesis_d" ))
-	void IPASpeechSynthesis_d(UPARAM(DisplayName = "Channel Id") FName InChannelId)
+	IPASpeechSynthesis IPASpeechSynthesis_d(UPARAM(DisplayName = "IPASpeechSynthesis") IPASpeechSynthesis InIPASpeechSynthesisObj, 
+		UPARAM(DisplayName = "Voice") FVoice InVoice)
 	{
-
+		return InIPASpeechSynthesisObj;
 	};
 	
 };
