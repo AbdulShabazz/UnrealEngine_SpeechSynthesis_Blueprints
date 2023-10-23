a70e2603fed5fd548dc7a0025a673aa6bcabadc3
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-10-23 08:07:05 -0400

    Create SpeechSynthesisBPLibrary.cpp

---
+++ Source/SpeechSynthesis/Private/SpeechSynthesisBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpeechSynthesisBPLibrary.h"
+#include "SpeechSynthesis.h"
+
+USpeechSynthesisBPLibrary::USpeechSynthesisBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USpeechSynthesisBPLibrary::SpeechSynthesisSampleFunction(float Param)
+{
+	return -1;
+}
+
