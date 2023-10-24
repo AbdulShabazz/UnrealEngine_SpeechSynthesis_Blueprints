6fc9c43f9b9c1f725722c8cd2ba874d3722d1b5e
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-10-24 01:08:11 -0400

    Create ProsodyCurve.h

---
+++ Source/SpeechSynthesisBP/Public/ProsodyCurve.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/AudioComponent.h"
+#include "ProsodyCurve.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPEECHSYNTHESISBP_API UProsodyCurve : public UAudioComponent
+{
+	GENERATED_BODY()
+	
+};
