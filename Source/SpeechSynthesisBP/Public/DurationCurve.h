9d1f1fd20c504264a1d34ff281ce47003f217a29
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-10-24 01:07:57 -0400

    Create DurationCurve.h

---
+++ Source/SpeechSynthesisBP/Public/DurationCurve.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/AudioComponent.h"
+#include "DurationCurve.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPEECHSYNTHESISBP_API UDurationCurve : public UAudioComponent
+{
+	GENERATED_BODY()
+	
+};
