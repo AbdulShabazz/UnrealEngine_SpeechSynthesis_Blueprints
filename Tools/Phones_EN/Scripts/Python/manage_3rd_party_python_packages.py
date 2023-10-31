66e37a1d80fbb6c947c4c3621302182428ba53f4
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-10-31 01:08:13 -0400

    Update manage_3rd_party_python_packages.py

--- Tools/Phones_EN/Scripts/Python/manage_3rd_party_python_packages.py
+++ Tools/Phones_EN/Scripts/Python/manage_3rd_party_python_packages.py
@@ -27,6 +27,32 @@ def manage_packages(action, packages):
             print(f"Failed to {action} {package}")
 
 if __name__ == "__main__":
+    comment = """
+    If an Error occurs during installation,
+    you can try installing packages individually
+
+   ``` bash
+    # Installing ffmpeg: Option 1 (Windows)
+    $ pip install ffmpeg-python
+
+    # Installing ffmpeg: Option 2 (Windows)
+    $ choco install ffmpeg-python
+
+    # Installing ffmpeg (Linux)
+    $ sudo apt update && sudo apt install ffmpeg-python
+    ```
+
+    ```bash
+    $ pip install numpy
+    $ pip install scipy
+    $ pip install matplotlib
+    $ pip install wave
+    $ pip install pydub
+    $ pip install torch
+    $ pip install torchaudio
+    $ pip install torchvision
+    ```
+    """
     packages = ["wave", "ffmpeg-python", "numpy", "scipy", "matplotlib", "pydub", "torch","torchaudio", "torchvision"]
     # manage_packages("install", packages)
     # manage_packages("uninstall", packages)
