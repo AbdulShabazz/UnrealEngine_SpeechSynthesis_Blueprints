43edc66c6bb73eb35597d759bcfbfe633f7d0807
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-11-02 14:54:09 -0400

    Create accent_common_names.py

---
+++ Tools/Phones_EN/accents/accent_common_names.py
@@ -0,0 +1,188 @@
+accent_common_names = {
+    "ad": "Joan/Maria",  # Andorra
+    "ae": "Mohammed/Fatima",  # United Arab Emirates
+    "al": "Gjergj/Mimoza",  # Albania
+    "am": "Davit/Ani",  # Armenia
+    "as": "John/Tala",  # American Samoa
+    "at": "Lukas/Sophie",  # Austria
+    "az": "Ali/Günel",  # Azerbaijan
+    "ba": "Amar/Aida",  # Bosnia and Herzegovina
+    "be": "Lucas/Emma",  # Belgium
+    "bf": "Moussa/Mariam",  # Burkina Faso
+    "bg": "Ivan/Maria",  # Bulgaria
+    "bi": "Pascal/Chantal",  # Burundi
+    "bj": "Koffi/Aissatou",  # Benin
+    "bs": "William/Shenique",  # Bahamas
+    "bt": "Tshering/Dema",  # Bhutan
+    "by": "Ivan/Anastasiya",  # Belarus
+    "ca": "Liam/Emma",  # Canada
+    "cat": "Jordi/Marta",  # Catalan
+    "cd": "Jean/Marie",  # Democratic Republic of the Congo
+    "cf": "Jean/Anne",  # Central African Republic
+    "cg": "Guy/Marie",  # Republic of the Congo
+    "ch": "Luca/Mia",  # Switzerland
+    "ci": "Koffi/Aminata",  # Ivory Coast
+    "cl": "Benjamín/Martina",  # Chile
+    "cm": "Joseph/Anne",  # Cameroon
+    "cn": "Wei/Jing",  # China
+    "co.ao": "Paulo/Maria",  # Angola (commercial)
+    "co.bw": "Oarabile/Neo",  # Botswana (commercial)
+    "co.ck": "John/Tina",     # Cook Islands (commercial)
+    "co.cr": "José/María",  # Costa Rica
+    "co.id": "Budi/Siti",  # Indonesia (commercial)
+    "co.il": "Amit/Tamar",  # Israel (commercial)
+    "co.in": "Raj/Priya",  # India (commercial)
+    "co.jp": "Hiroshi/Yuko",  # Japan (commercial)
+    "co.ke": "John/Wangui",  # Kenya (commercial)
+    "co.kr": "Min-Jun/Ji-Eun",  # South Korea (commercial)
+    "co.ls": "Thabo/Masego",  # Lesotho (commercial)
+    "co.ma": "Youssef/Fatima",  # Morocco (commercial)
+    "co.mz": "João/Ana",  # Mozambique (commercial)
+    "co.nz": "Jack/Sophie",  # New Zealand (commercial)
+    "co.th": "Somchai/Suda",  # Thailand (commercial)
+    "co.tz": "Juma/Mariam",  # Tanzania (commercial)
+    "co.ug": "Moses/Nakato",  # Uganda (commercial)
+    "co.uk": "Oliver/Olivia",  # United Kingdom (commercial)
+    "co.uz": "Oleg/Nigora",  # Uzbekistan (commercial)
+    "co.ve": "José/Daniela",  # Venezuela (commercial)
+    "co.vi": "John/Lisa",  # U.S. Virgin Islands (commercial)
+    "co.za": "James/Jessica",  # South Africa (commercial)
+    "co.zm": "John/Temwani",  # Zambia (commercial)
+    "co.zw": "Tinashe/Rutendo",  # Zimbabwe (commercial)
+    "com.af": "Ahmad/Fatima",  # Afghanistan (commercial)
+    "com.ag": "Michael/Elizabeth",  # Antigua and Barbuda (commercial)
+    "com.ar": "Santiago/Sofía",  # Argentina (commercial)
+    "com.au": "Jack/Charlotte",  # Australia (commercial)
+    "com.bd": "Mohammad/Sanjida",  # Bangladesh (commercial)
+    "com.bh": "Ali/Noor",  # Bahrain (commercial)
+    "com.bn": "Mohammad/Siti",  # Brunei (commercial)
+    "com.bo": "Juan/Luz",  # Bolivia (commercial)
+    "com.br": "João/Ana",  # Brazil (commercial)
+    "com.bz": "John/Elizabeth",  # Belize (commercial)
+    "com.co": "Carlos/Sofia",  # Colombia (commercial)
+    "com.cu": "Alejandro/Maria",  # Cuba (commercial)
+    "com.cy": "Andreas/Maria",  # Cyprus (commercial)
+    "com.do": "Luis/Maria",  # Dominican Republic (commercial)
+    "com.ec": "José/María",  # Ecuador (commercial)
+    "com.eg": "Mohamed/Fatma",  # Egypt (commercial)
+    "com.et": "Daniel/Meron",  # Ethiopia (commercial)
+    "com.fj": "Apenisa/Adi",  # Fiji (commercial)
+    "com.gh": "Kwame/Ama",  # Ghana (commercial)
+    "com.gi": "John/Karen",  # Gibraltar (commercial)
+    "com.gt": "Juan/María",  # Guatemala (commercial)
+    "com.hk": "Chi/Mei",  # Hong Kong (commercial)
+    "com.jm": "Michael/Karen",  # Jamaica (commercial)
+    "com.kh": "Sok/Sophea",  # Cambodia (commercial)
+    "com.kw": "Ahmad/Mariam",  # Kuwait (commercial)
+    "com.lb": "Mohammad/Rana",  # Lebanon (commercial)
+    "com.ly": "Mohammed/Amna",  # Libya (commercial)
+    "com.mm": "Aung/Khin",  # Myanmar (commercial)
+    "com.mt": "Joseph/Maria",  # Malta (commercial)
+    "com.mx": "José/María",  # Mexico (commercial)
+    "com.my": "Ahmad/Siti",  # Malaysia (commercial)
+    "com.na": "John/Maria",  # Namibia (commercial)
+    "com.ng": "Chinedu/Amaka",  # Nigeria (commercial)
+    "com.ni": "José/María",  # Nicaragua (commercial)
+    "com.np": "Raj/Kumari",  # Nepal (commercial)
+    "com.om": "Ahmed/Fatma",  # Oman (commercial)
+    "com.pa": "José/María",  # Panama (commercial)
+    "com.pe": "Juan/María",  # Peru (commercial)
+    "com.pg": "John/Anna",  # Papua New Guinea (commercial)
+    "com.ph": "Jose/Maria",  # Philippines (commercial)
+    "com.pk": "Muhammad/Aisha",  # Pakistan (commercial)
+    "com.pr": "Luis/Maria",  # Puerto Rico (commercial)
+    "com.py": "Carlos/María",  # Paraguay (commercial)
+    "com.qa": "Mohammed/Noor",  # Qatar (commercial)
+    "com.sa": "Mohammed/Sarah",  # Saudi Arabia (commercial)
+    "com.sb": "John/Mary",  # Solomon Islands (commercial)
+    "com.sg": "Wei/Ming/Li",  # Singapore (commercial)
+    "com.sl": "Mohamed/Fatmata",  # Sierra Leone (commercial)
+    "com.sv": "José/María",  # El Salvador (commercial)
+    "com.tj": "Muhammad/Farzona",  # Tajikistan (commercial)
+    "com.tr": "Ahmet/Fatma",  # Turkey (commercial)
+    "com.tw": "Chen/Yu",  # Taiwan (commercial)
+    "com.ua": "Oleksandr/Anna",  # Ukraine (commercial)
+    "com.uy": "Juan/Ana",  # Uruguay (commercial)
+    "com.vc": "Ronald/Nicole",  # Saint Vincent and the Grenadines (commercial)
+    "com.vn": "Nguyen/Van",  # Vietnam (commercial)
+    "cv": "José/Fatima",  # Cape Verde
+    "cz": "Jan/Eliška",  # Czech Republic
+    "de": "Max/Sophia",  # Germany
+    "dj": "Abdi/Samira",  # Djibouti
+    "dk": "Noah/Sofia",  # Denmark
+    "dm": "Michael/Elizabeth",  # Dominica
+    "dz": "Mohamed/Sarah",  # Algeria
+    "ee": "Martin/Liisa",  # Estonia
+    "es": "Antonio/María",  # Spain
+    "fi": "Juhani/Maria",  # Finland
+    "fm": "John/Jane",  # Micronesia
+    "fr": "Louis/Emma",  # France
+    "ga": "Jean/Marie",  # Gabon
+    "ge": "Giorgi/Mariam",  # Georgia
+    "gg": "John/Sarah",  # Guernsey
+    "gl": "Johan/Anna",  # Greenland
+    "gm": "Omar/Fatou",  # Gambia
+    "gr": "George/Maria",  # Greece
+    "gy": "John/Susan",  # Guyana
+    "hn": "Carlos/Sofia",  # Honduras
+    "hr": "Ivan/Ana",  # Croatia
+    "ht": "Jean/Marie",  # Haiti
+    "hu": "László/Anna",  # Hungary
+    "ie": "Conor/Emily",  # Ireland
+    "im": "John/Mary",  # Isle of Man
+    "iq": "Mohammed/Fatima",  # Iraq
+    "is": "Jón/Anna",  # Iceland
+    "it": "Matteo/Sofia",  # Italy
+    "je": "John/Sophie",  # Jersey
+    "jo": "Mohammed/Aya",  # Jordan
+    "kg": "Nurislam/Aigul",  # Kyrgyzstan
+    "ki": "Tetabo/Teriaki",  # Kiribati
+    "kz": "Nursultan/Aigerim",  # Kazakhstan
+    "la": "Bounnhang/Manivanh",  # Laos
+    "li": "Hans/Andrea",  # Liechtenstein
+    "lk": "Mohamed/Sandya",  # Sri Lanka
+    "lt": "Lukas/Gabija",  # Lithuania
+    "lu": "Jean/Claire",  # Luxembourg
+    "lv": "Jānis/Līga",  # Latvia
+    "md": "Ion/Ana",  # Moldova
+    "me": "Marko/Jelena",  # Montenegro
+    "mg": "Andry/Voahangy",  # Madagascar
+    "mk": "Aleksandar/Ana",  # North Macedonia
+    "ml": "Mohamed/Aminata",  # Mali
+    "mn": "Bat-Erdene/Oyungerel",  # Mongolia
+    "mu": "Jean/Marie",  # Mauritius
+    "mv": "Mohamed/Mariyam",  # Maldives
+    "mw": "John/Mary",  # Malawi
+    "ne": "Moussa/Hadiza",  # Niger
+    "nl": "Daan/Emma",  # Netherlands
+    "no": "Lukas/Nora",  # Norway
+    "nr": "Aloysius/Timaima",  # Nauru
+    "nu": "Togi/Einat",  # Niue
+    "pl": "Jakub/Zuzanna",  # Poland
+    "pn": "William/Mary",  # Pitcairn Islands
+    "ps": "Mohammed/Samah",  # Palestinian territories
+    "pt": "João/Maria",  # Portugal
+    "ro": "Andrei/Maria",  # Romania
+    "rs": "Nikola/Jovana",  # Serbia
+    "ru": "Alexander/Anastasia",  # Russia
+    "rw": "Jean/Diane",  # Rwanda
+    "sc": "Jean/Linda",  # Seychelles
+    "se": "Liam/Alice",  # Sweden
+    "sh": "John/Sharon",  # Saint Helena
+    "si": "Luka/Ana",  # Slovenia
+    "sk": "Jakub/Sofia",  # Slovakia
+    "sm": "Matteo/Giulia",  # San Marino
+    "sn": "Moussa/Aminata",  # Senegal
+    "so": "Abdi/Fatima",  # Somalia
+    "sr": "Rajiv/Sandra",  # Suriname
+    "st": "João/Maria",  # São Tomé and Príncipe
+    "td": "Mahamat/Amina",  # Chad
+    "tg": "Koffi/Afi",  # Togo
+    "tl": "João/Maria",  # Timor-Leste
+    "tm": "Ata/Serdar/Gul",  # Turkmenistan
+    "tn": "Mohamed/Amira",  # Tunisia
+    "to": "Sione/Ana",  # Tonga
+    "tt": "Brian/Michelle",  # Trinidad and Tobago
+    "vu": "Jean/Marie",  # Vanuatu
+    "ws": "Ioane/Sina",  # Samoa
+}
