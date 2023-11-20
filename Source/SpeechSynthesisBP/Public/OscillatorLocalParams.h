a8aab4d2a871376283432933ea8a57dfe6c70835
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-11-20 03:38:07 -0500

    Create OscillatorLocalParams.h

---
+++ Source/SpeechSynthesisBP/Public/OscillatorLocalParams.h
@@ -0,0 +1,48 @@
+#pragma once
+
+// forward declarations
+enum class WaveShape : uint32_t;
+
+struct TimeInterval {
+    double value;
+    explicit TimeInterval(double val) : value(val) {}
+};
+
+/**
+@details C++20 concept to check if two types are bitwise operations compatible.
+@tparam T The first type.
+@tparam U The second type.*/
+template <typename T>
+concept bitwise_ops_compatible_shapeType = requires( T t ) {
+    static_cast<uint32_t>( t ) & static_cast<uint32_t>( 1 );
+    static_cast<uint32_t>( t ) | static_cast<uint32_t>( 1 );
+};
+
+/**
+@brief An OscillatorParameters class used to configure formant signals chains.
+@details An OscillatorParameters class used to configure formant signals chains. A C++20 concept to check if two types are bitwise operations compatible.
+*/
+template <
+    typename AmplitudeType = double,
+    typename FrequencyHzType = double,
+    typename ThetaType = double,
+    typename ShapeType = WaveShape,
+    typename OutputShapeType = double,
+    typename TimeStepType = double
+>
+requires ( bitwise_ops_compatible_shapeType<ShapeType> )
+class OscillatorParameters {
+public:
+    AmplitudeType amplitude{};
+    FrequencyHzType frequencyHz{};
+    ThetaType theta{};
+    ShapeType shape{};
+    OutputShapeType outputShape{};
+    TimeStepType timeStepStart{};
+    TimeStepType timeStepEnd{};
+    TimeInterval timeInterval{}; 
+    /*bool isaComplexWaveShape_flag{};*/
+    bool usesTimeInterval_flag{};
+
+    // Constructor, getters, setters, etc. as needed
+};
