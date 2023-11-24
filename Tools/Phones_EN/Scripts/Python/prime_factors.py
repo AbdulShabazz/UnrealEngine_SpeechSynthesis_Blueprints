dc18ae33c930414d402f661b9899608ded2dc061
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-11-24 15:44:50 -0500

    Create prime_factors.py

---
+++ Tools/Phones_EN/Scripts/Python/prime_factors.py
@@ -0,0 +1,14 @@
+
+# Function to find the prime factors of a number
+def prime_factors(n: int):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
