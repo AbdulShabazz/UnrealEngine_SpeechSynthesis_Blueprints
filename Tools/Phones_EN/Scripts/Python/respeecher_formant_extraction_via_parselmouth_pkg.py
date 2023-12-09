4c8b665e7e3e914570f17aacf004506de57ddb44
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-12-09 00:15:29 -0500

    Delete respeecher_formant_extraction_via_parselmouth_pkg.py

--- Tools/Phones_EN/Scripts/Python/respeecher_formant_extraction_via_parselmouth_pkg.py
+++
@@ -1,6 +0,0 @@
-import parselmouth
-
-# Extract formants from an audio file
-snd = parselmouth.Sound("combined_sine_waves.wav")
-formants = snd.to_formant_burg()
-formants.save_as_text_file("combined_sine_waves.formants")
