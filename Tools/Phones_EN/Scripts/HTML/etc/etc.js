3b67065328b24b60ab56fea7abc0afcc9f29b7d7
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-12-24 02:42:55 -0500

    Create etc.js

---
+++ Tools/Phones_EN/Scripts/HTML/etc/etc.js
@@ -0,0 +1,24 @@
+// Event listeners for dropdowns
+document.getElementById('frequency-selector').addEventListener('change', function() {
+    // Update chart data
+  });
+  
+  // Event listeners for dropdowns
+  document.getElementById('amplitude-selector').addEventListener('change', function() {
+    // Update chart data
+  });
+  
+  // Event listeners for dropdowns
+  document.getElementById('motif-selector').addEventListener('change', function() {
+    // Update chart data
+  });
+  
+  // Event listeners for dropdowns
+  document.getElementById('samples-selector').addEventListener('change', function() {
+    // Update chart data
+  });
+
+document.querySelector('select[id="amplitude-selector"]').value = '0';
+document.querySelector('select[id="frequency-selector"]').value = '1';
+document.querySelector('select[id="motif-selector"]').value = '3';
+document.querySelector('select[id="samples-selector"]').value = '2';
