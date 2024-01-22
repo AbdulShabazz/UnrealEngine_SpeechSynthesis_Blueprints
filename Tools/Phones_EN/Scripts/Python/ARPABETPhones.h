53b298bcf02aeaeccf6400bdc927365f83acd0e6
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2024-01-22 08:30:06 -0500

    Create ARPABETPhones.h

---
+++ Tools/Phones_EN/Scripts/Python/ARPABETPhones.h
@@ -0,0 +1,282 @@
+
+#pragma once
+
+// This file is the Sara voice definition file. Todo: reimport as a .DLL
+
+#include "FPhone.h"
+#include "oscillatorConfig.h"
+
+namespace ARPABETPhone
+{
+
+    class ARPABETPhone_AX : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_EY : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_AE : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_DX : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_IY : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_EH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_ER : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_IH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_OW : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_AA : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_UH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_UW : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_AO : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_OY : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_AH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_AY : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_EL : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_CH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_D : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_AW : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_F : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_G : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_HH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_JH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_K : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_L : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_ZH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_M : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_N : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_P : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_R : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_IX : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_SH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_NX : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_S : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_EM : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_EN : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_T : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_DH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_TH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_V : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_W : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_WH : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_Y : public FPhone
+    {
+    public:
+
+    };
+
+    class ARPABETPhone_Z : public FPhone
+    {
+    public:
+
+    };
+
+}
