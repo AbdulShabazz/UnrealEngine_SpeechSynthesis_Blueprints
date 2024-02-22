ffebca27cbe8d8e56fb917e14f40e30dd469e9d8
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2024-02-22 11:02:53 -0500

    Create etc.cpp

---
+++ Tools/Phones_EN/Scripts/HTML/etc/etc.cpp
@@ -0,0 +1,18 @@
+
+//#include <unordered_map>
+//#include <functional>
+
+            /*
+            const bool interval_has_3_control_points = ((idx + 2) < pts.size());
+            const bool interval_has_2_control_points = ((idx + 1) < pts.size());
+            
+            if (interval_has_3_control_points
+            && iframe >= pts[idx + 2].frame) ++iframe;
+
+            else if (interval_has_2_control_points
+            && iframe >= pts[idx + 1].frame) ++iframe;
+            */
+            // if (saturatedFlag_a) ++iframe;
+            // if (iframe >= pts[idx].frame) ++idx;
+            //if (saturatedFlag_f) ++iframe;
+            //if (iframe >= pts[idx].frame) ++idx;
